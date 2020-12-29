8137921f6475aa8273591547a3b2d9ace15c6436
Cytaa <piotrstachnio@gmail.com>
2020-12-29 14:50:47 +0100

    used imap to fetch mails

--- mailClient.py
+++ mailClient.py
@@ -1,1 +1,20 @@
-print("chuj")
+import json
+import smtplib
+import imaplib
+
+with open("data.json") as jsonFile:
+    jsonObject = json.load(jsonFile)
+    jsonFile.close()
+
+try:
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login(jsonObject["mail"], jsonObject["password"])
+    mail.select("inbox")
+
+    for i in range(1, 10):
+        data = mail.fetch(str(i), "(RFC822)")
+        print(str(data) + "\n")
+
+
+except Exception as e:
+    print(str(e))
